47ef9332a707b8545e121b419674114ca3295ec6
Purplix.co <dannilosigma@gmail.com>
2026-07-31 19:46:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
+import os
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = "8849624925:AAF01JH8vA8atOT1fDG75CQStqXwagLwehA"
+TOKEN = os.environ["8849624925:AAF01JH8vA8atOT1fDG75CQStqXwagLwehA"]
 bot = telebot.TeleBot(TOKEN)
 
+# ==========================
+# СКРИПТЫ
+# ==========================
+scripts = {
+    "mm2": """```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xv3gasx/Murder-Mystery-2/refs/heads/main/Release.lua"))()
+```""",
+
+    "script2": """```lua
+-- Вставь сюда второй скрипт
+```""",
+
+    "script3": """```lua
+-- Вставь сюда третий скрипт
+```"""
+}
+
+# ==========================
+# /start
+# ==========================
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(message, "Привет!")
+    args = message.text.split(maxsplit=1)
 
+    if len(args) > 1 and args[1] in scripts:
+
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(
+                "📢 Наш Telegram-канал",
+                url="https://t.me/DeltaWScripts"
+            )
+        )
+
+        bot.send_message(
+            message.chat.id,
+            f"✅ Вот твой скрипт:\n\n{scripts[args[1]]}",
+            parse_mode="Markdown",
+            reply_markup=keyboard
+        )
+
+    else:
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(
+                "📢 Наш Telegram-канал",
+                url="https://t.me/DeltaWScripts"
+            )
+        )
+
+        bot.send_message(
+            message.chat.id,
+            "👋 Добро пожаловать!\n\nПерейди в канал и нажми кнопку «Получить скрипт».",
+            reply_markup=keyboard
+        )
+
+print("Бот запущен!")
 bot.infinity_polling()
